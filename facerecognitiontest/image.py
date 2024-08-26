b4d503067fcdba69803f080949d06989252d1c13
uPiscium <upiscium@gmail.com>
2024-08-26 14:12:49 +0900

    Add comments.

--- facerecognitiontest/image.py
+++ facerecognitiontest/image.py
@@ -1,14 +1,23 @@
 import face_recognition as FaceRecognition
-import cv2
 import numpy
 import os
 
 
 class ImageLoader:
+    """
+    画像を読み込み、顔のエンコーディングを取得し、データを保存するクラス
+    """
+
     def __init__(self) -> None:
-        self.images: dict[str, numpy.ndarray] = {}
+        self.images: dict[str, numpy.ndarray] = {}  # 画像の名前とエンコーディングの辞書
 
     def LoadImageFile(self, name: str, path: str) -> None:
+        """
+        name: 写真に対応する名前
+        path: 画像のパス
+
+        画像を読み込み、顔のエンコーディングを取得し、データをself.imagesに保存する
+        """
         img = FaceRecognition.load_image_file(path)
         if len(FaceRecognition.face_encodings(img)) == 0:
             raise ValueError("No face found in the image")
@@ -19,9 +28,21 @@ class ImageLoader:
         self.images[name] = FaceRecognition.face_encodings(img)[0]
 
     def GetFiles(self, directory: str) -> list[str]:
+        """
+        directory: ディレクトリのパス
+
+        ディレクトリ内のファイルのパスを取得する
+        この関数はローカル環境でのテスト実行時にモックするために使用されます
+        """
         return [f"{directory}/{path}" for path in os.listdir(directory)]
 
     def GenerateFaceSetFromDirectory(self, directory: str) -> None:
+        """
+        directory: 画像が保存されているディレクトリのパス
+
+        ディレクトリ内の画像を読み込み、顔のエンコーディングを取得し、データを保存する
+        この関数はローカル環境でのテスト実行時にモックするために使用されます
+        """
         """This function is used for mocking..."""
         for path in self.GetFiles(directory):
             name = path.split("/")[-1].split(".")[0]
